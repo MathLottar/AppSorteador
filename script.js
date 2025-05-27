345183d0d529bf1d3cad607e6b722c95801f480c
MathLottar <matheus-lottar@outlook.com>
2025-05-27 10:44:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/balancear', {
+            const response = await fetch('https://appsorteador.onrender.com/balancear', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
